feb801ba4546e2bf1c9211249c91984fd098d855
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2021-11-02 20:42:35 +0000

    IP details inner html

--- app/js/server.js
+++ app/js/server.js
@@ -9,3 +9,22 @@ async function getDomain() {
 getDomain().then(data => console.log(data));
 
 
+
+const updateUI = async() => {
+    const req = await fetch('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_VMqlSxLK7yy6QSSGbqGSwifDqAada');
+    try{
+        const allData = await req.json();
+        document.getElementById('ip-address').innerHTML = `${allData.ip}`;
+        document.getElementById('city').innerHTML = `${allData.location.city}`;
+        document.getElementById('region').innerHTML = `${allData.location.region}`;
+        document.getElementById('postalCode').innerHTML = `${allData.location.postalCode}`;
+        document.getElementById('timezone').innerHTML = `${allData.location.timezone}`;
+        document.getElementById('isp').innerHTML = `${allData.isp}`;
+        
+    }catch(error){
+        console.log("error", error);
+    }
+}
+updateUI();
+
+//81.109.105.122

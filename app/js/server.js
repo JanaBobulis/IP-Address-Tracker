f2cc5c0765182cbbcbc886bb2db72e219d32577d
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2021-11-03 22:51:36 +0000

    ip address search functionality

--- app/js/server.js
+++ app/js/server.js
@@ -1,30 +1,75 @@
-//get request
+var mymap = L.map('map').setView([51.505, -0.09], 13);
 
-async function getDomain() {
-    let response = await fetch('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_VMqlSxLK7yy6QSSGbqGSwifDqAada');
-    let data = await response.json();
-    return data; 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiamFuYWJvYnVsaXMiLCJhIjoiY2t2aDF6bGEyMjgzOTJwcGd1OHhqdW1zYyJ9.otvWt14x89J84qEYp4l3ug'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+let baseURL = "https://geo.ipify.org/api/v2/";
+let apiKey = ""
+
+
+let ipAddress = document.getElementById('ip-address');
+let city = document.getElementById('city');
+let region = document.getElementById('region');
+let postalCode = document.getElementById('postalCode');
+let timeZone = document.getElementById('timezone');
+let isp = document.getElementById('isp');
+let enteredIp = document.getElementById('ip-search');
+
+// API get request
+// async function getDomain() {
+//     let response = await fetch('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_VMqlSxLK7yy6QSSGbqGSwifDqAada');
+//     let data = await response.json();
+//     return data; 
+// }
+
+// getDomain().then(data => console.log(data));
+
+const updateUI = (ip, domain) => {
+    console.log(ip, domain)
+     fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_VMqlSxLK7yy6QSSGbqGSwifDqAada&ipAddress=${ip}`)
+     
+    .then (results => results.json())
+    .then (data => {
+        ipAddress.innerHTML = `${data.ip}`;
+        city.innerHTML = `${data.location.city}`;
+        region.innerHTML = `${data.location.region}`;
+        postalCode.innerHTML = `${data.location.postalCode}`;
+        timeZone.innerHTML = `${data.location.timezone}`;
+        isp.innerHTML = `${data.isp}`;
+
+        let lng = data.location.lng;
+        let lat = data.location.lat;
+
+        marker.setLatLng([lat, lng]).update();  // Updates your defined marker position
+        mymap.setView([lat, lng]); // Updates map position
+    })
+    .catch(error => {
+        alert('unable to get IP details')
+        console.log(error)
+    })
 }
 
-getDomain().then(data => console.log(data));
+document.getElementById("submit-btn").addEventListener("click", performAction);
 
-
-
-const updateUI = async() => {
-    const req = await fetch('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_VMqlSxLK7yy6QSSGbqGSwifDqAada');
-    try{
-        const allData = await req.json();
-        document.getElementById('ip-address').innerHTML = `${allData.ip}`;
-        document.getElementById('city').innerHTML = `${allData.location.city}`;
-        document.getElementById('region').innerHTML = `${allData.location.region}`;
-        document.getElementById('postalCode').innerHTML = `${allData.location.postalCode}`;
-        document.getElementById('timezone').innerHTML = `${allData.location.timezone}`;
-        document.getElementById('isp').innerHTML = `${allData.isp}`;
-        
-    }catch(error){
-        console.log("error", error);
+function performAction(e) {
+    e.preventDefault();
+    if (enteredIp.value != '' && enteredIp.value != null) {
+        updateUI(enteredIp.value);
+        return;
+    } else {
+        alert("Please enter a valid IP address")
     }
 }
-updateUI();
+
+
+
 
 //81.109.105.122
